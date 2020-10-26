96f6cd422aebeecf0efc55d0086b3a95084d1539
guanh <guanh@192.168.10.74>
2020-10-26 11:54:44 +0800

    批量导出demo

--- xuanYue-seach/src/main/java/com/xuanyue/db/xuan/Hello.java
+++ xuanYue-seach/src/main/java/com/xuanyue/db/xuan/Hello.java
@@ -35,13 +35,13 @@ public class Hello {
 		phs.setSource(80);
 		dbMeta.add(tableName, phs);
 		
-		phs.addColumn(UNumberIndex.class, "source_no",44  );
-		phs.addColumn(ListUnumberIndex.class, "business_labels" ,10,12 );
-		phs.addColumn(UNumberIndex.class, "province" ,6 );
-		phs.addColumn(UNumberIndex.class, "city" ,14 );
-		phs.addColumn(UNumberIndex.class, "operator" ,4 );
-		phs.addColumn(ListUnumberIndex.class, "game_labels" ,10,16 );
-		phs.addColumn(DateIndex.class, "create_time" );//35
+		phs.addColumn(UNumberIndex.class, "source_no",35  );//源发号码  范围： 0-34,359,738,368
+		phs.addColumn(ListUnumberIndex.class, "business_labels" ,10,12 );//营销标签 10个 整数  范围：  0-4095之间的数
+		phs.addColumn(UNumberIndex.class, "province" ,6 );//省  范围：  0-63 直接
+		phs.addColumn(UNumberIndex.class, "city" ,14 );//城市   范围：   0-16383之间
+		phs.addColumn(UNumberIndex.class, "operator" ,4 );//运营商    范围：  0-16之间
+		phs.addColumn(ListUnumberIndex.class, "game_labels" ,10,16 );//游戏标签    范围： 0-65535
+		phs.addColumn(DateIndex.class, "create_time" );//创建时间
 		
 		IXyDB db = new XyDB();
 		db.init(dbMeta);
@@ -50,6 +50,8 @@ public class Hello {
 		IXyTable table = db.getTable(tableName);
 		//table.flush(10000000);
 		table.toBatchLoadMode(String.format("%s/%s/%s",dbMeta.getDataPath(),dbMeta.getName(),  tableName));
+		
+		//模拟数据
 		Map<String,Object> record = new HashMap<>();
 		record.put("source_no", 13521192609l);
 		
@@ -76,21 +78,19 @@ public class Hello {
 		gl.add(33l);
 		record.put("game_labels", gl);
 		record.put("create_time", new Date());
+		
 		long s = System.currentTimeMillis();
-		add(record,table,num);
-		//table.insertInto(record);//插入
-		table.save(String.format("%s/%s/%s",dbMeta.getDataPath(),dbMeta.getName(),  tableName));
-		System.out.println(System.currentTimeMillis()-s);
-		System.out.println(table.read(2));
-	}
-	static void add(Map<String,Object> record,IXyTable table,int num)throws Exception{
-		for(int i=0;i<num;i++) {
-			record.put("source_no", (long)record.get( "source_no" )+1l );
+		for(int i=0;i<num;i++) {//插入num次
+			record.put("source_no", (long)record.get( "source_no" )+1l );//每次 source_no加1
 			table.insertInto(record);
 			if(i%100000==99999) {
 				System.out.println(i);
 			}
 		}
+		//table.insertInto(record);//插入
+		table.save(String.format("%s/%s/%s",dbMeta.getDataPath(),dbMeta.getName(),  tableName));
+		System.out.println(System.currentTimeMillis()-s);
+		System.out.println(table.read(2));
 	}
 	static void test(String path)throws Exception {
 		DBMeta dbMeta = new DBMeta();
@@ -146,7 +146,6 @@ public class Hello {
 	public static void main(String[] args) throws Exception {
 		//test();
 		long s = System.currentTimeMillis();
-		//init(100000,"d:/data2");
 		test("d:/data2");
 		long e = System.currentTimeMillis();
 		System.out.println(e-s);
