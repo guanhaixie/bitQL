35aab0ca3c066bfe7e25dfe65f8ba745704b0e49
guanh <guanh@192.168.10.74>
2020-10-29 16:54:01 +0800

    删除无用的类

--- xuanYue-utils/src/main/java/com/xuanyue/db/xuan/utils/io/InputStreamSource.java
+++
@@ -1,8 +0,0 @@
-package com.xuanyue.db.xuan.utils.io;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-public interface InputStreamSource {
-	InputStream getInputStream() throws IOException;
-}
